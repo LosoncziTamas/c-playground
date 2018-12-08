746b6f2212d7c9531a191de4deddc12fb84c96e4
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-08 10:19:04 +0100

    add recursive string length function and some dummy array/string code

--- string2.c
+++ string2.c
@@ -44,6 +44,16 @@ size_t length2(char *str)
     return len;
 }
 
+size_t length3(char *str)
+{
+    size_t size = 0;
+    if ((*str) != '\0') {
+        ++size;
+        size += length3(++str);
+    }
+    return size;
+}
+
 int main(void)
 {
     char *str = "This is a string";
@@ -60,6 +70,23 @@ int main(void)
     printf("\n");
 
     printf("%zu", length2(str2));
+    printf("\n");
+
+    printf("%zu", length3(str));
+    printf("\n");
+
+    // If the string's length is less to the array's length then the array is filled with the char elements + null terminator
+    char str3[20] = "This is a string";
+    size_t arrayLength = sizeof(str3) / sizeof(str3[0]);
+    size_t strLength = length(str3);
+    printf("array length: %zu, string length: %zu", arrayLength, strLength);
+    printf("\n");
+
+    // If the string's length is more or equal to the array's length then the null terminator will not be appended.
+    // So printf will print until it finds a terminator or zero.
+    char str4[16] = "This is a string";
+    
+    printf("%s", str4);
     printf("\n");
 
     return 0;
