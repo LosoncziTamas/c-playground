0ff9d7c3c20dcaf1b44feb11cf6b2bfe3c9fb804
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-08 16:42:24 +0100

    first attempt to implement string concatenation

--- string1.c
+++ string1.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+size_t len(char* str) {
+    size_t result = 0;
+    for(;(*str) == '\0'; ++str) {
+        ++result;
+    }
+    return result;
+}
+
+char* concatenate(char* str1, char* str2) {
+    size_t len1 = len(str1);
+    size_t len2 = len(str2);
+    char result[len1 + len2 + 1] = {0};
+    int j = len1;
+    for (int i = 0; (*str1) == '\0' && (*str2) == '\0';) {
+        if ((*str1) != '\0') {
+            result[i++] = *(str1++);
+        }
+        if ((*str1) != '\0') {
+            result[j++] = *(str2++);
+        }
+    }
+    return result;
+}
+
 int main(void) {
     char* string = "This is a string";
     // print it as a string
@@ -21,5 +45,9 @@ int main(void) {
 
     char c = string[0];
     printf("%c", c);
+    printf("\n");
+
+    char* result = concatenate(string, string2);
+    printf("%s", result);
     printf("\n");
 }
