e7b83f2ec65bb74310b5e860aa32d40adc66b97e
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-09 15:15:37 +0100

    second attempt to implement string concatenation

--- string1.c
+++ string1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t len(char* str) {
     size_t result = 0;
@@ -8,21 +9,30 @@ size_t len(char* str) {
     return result;
 }
 
+void copyString(char* from, char* to) {
+    while ((*from) != '\0') {
+        *(to++) = *(from++);
+    }    
+}
+
 char* concatenate(char* str1, char* str2) {
     size_t len1 = len(str1);
     size_t len2 = len(str2);
-    char result[len1 + len2 + 1] = {0};
+    char *result = malloc((len1 + len2 + 1) * sizeof(char));
     int j = len1;
-    for (int i = 0; (*str1) == '\0' && (*str2) == '\0';) {
+    for (int i = 0; (*str1) != '\0' && (*str2) != '\0';) {
         if ((*str1) != '\0') {
+            printf("%c", *str1);
             result[i++] = *(str1++);
         }
         if ((*str1) != '\0') {
+            printf("%c", *str2);
             result[j++] = *(str2++);
         }
     }
     return result;
 }
+
 
 int main(void) {
     char* string = "This is a string";
@@ -35,8 +45,8 @@ int main(void) {
     // increment the pointer & dereference and print character
     printf("%c", *(++string));
     printf("\n");
-    // print the the string again
-    printf("%s", string);
+    // print the the string again, but from the second char and reset it to first
+    printf("%s", string--);
     printf("\n");
 
     char string2[] = "This is a string";
@@ -47,7 +57,13 @@ int main(void) {
     printf("%c", c);
     printf("\n");
 
-    char* result = concatenate(string, string2);
-    printf("%s", result);
+    char str3[17] = {0}; 
+    copyString(string, str3);
+    printf("%s", str3);
+    printf("\n");
+
+    char* string4 = "1234";
+    char c2 = *(string4++);
+    printf("%c", c2);
     printf("\n");
 }
