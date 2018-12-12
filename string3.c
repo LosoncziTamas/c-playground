293b828516f53670c826caee8c1357b71981dc2b
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-12 22:30:36 +0100

    improve string reverse

--- string3.c
+++ string3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t stringLength(const char *str)
 {
@@ -10,24 +11,54 @@ size_t stringLength(const char *str)
     return result;
 }
 
-void reverseString(char *str)
+void copyString(const char* src, size_t srcLen, char* dest) 
+{
+    while(srcLen--) 
+    {
+        *(dest++) = *(src++);
+    }
+}
+
+char* duplicateString(const char* str) 
+{
+    size_t len = stringLength(str);
+    char* result = malloc((len + 1) * sizeof(char));
+    copyString(str, len, result);
+    return result;
+}
+
+void reverseStringInPlace(char *str)
 {
     size_t len = stringLength(str);
     for (int i = 0; i < (len / 2); ++i)
     {
         char tmp = str[i];
         str[i] = str[len - (i + 1)];
-        str[len - i] = tmp;
+        str[len - (i + 1)] = tmp;
     }
     str[len] = '\0';
+}
+
+char* reverseString(const char* str) 
+{
+    char* result = duplicateString(str);
+    printf("duplicate result: %s", result);
+    printf("\n");
+    reverseString(result);
+    return result;
 }
 
 int main(void)
 {
     char str[] = "Hello, world";
-    reverseString(str);
+    reverseStringInPlace(str);
     printf("%s", str);
     printf("\n");
+
+    char* newStr = reverseString(str);
+    printf("%s", newStr);
+    printf("\n");
+    free(newStr);
 
     return 0;
 }
