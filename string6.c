812765c47b65a9d1d5e77fb022c0f508b33f9b8e
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-16 18:55:17 +0100

    playing with character cases

--- string6.c
+++ string6.c
@@ -7,6 +7,39 @@ char toUpperCase(char c) {
     return c;
 }
 
+char toLowerCase(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        c += 'a' - 'A';
+    }
+    return c;
+}
+
+char invertCase(char c) {
+    if (c >= 'A' && c <= 'z') {
+        c ^= 1 << 5;
+    }
+    return c;
+}
+
+// ASCII is 7 bit character set
+void printAscii() {
+    for(unsigned char i = 0; i < 128; ++i) {
+        printf("%d: %c ", i, i);
+        if (i % 10 == 0) {
+            printf("\n");
+        }
+    }
+}
+
+void printNonAscii() {
+    for(int i = 128; i < 256; ++i) {
+        printf("%d: %c ", i, i);
+        if (i % 10 == 0) {
+            printf("\n");
+        }
+    }
+}
+
 int main(void) {
 
     // ASCII value of 'a' is 97
@@ -15,9 +48,15 @@ int main(void) {
     char upperCase = 'A';
 
     char diff = 'a' - 'A';
-
     printf("%d %d %d\n", lowerCase, upperCase, diff);
+
+    printAscii();
+    printNonAscii();
+
     printf("%c", toUpperCase('b'));
+    printf("%c", toLowerCase('B'));
+    printf("%c", invertCase('A'));
+    printf("%c", invertCase('a'));
 
     return 0;
 }
