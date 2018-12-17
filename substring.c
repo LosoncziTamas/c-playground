8c2c9a5f8ff97c57bb512b7048c7f7a2e5f50114
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-17 21:30:16 +0100

    rename files & add substring

---
+++ substring.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+char* substr(const char* str, unsigned int start, unsigned int end) {
+    assert(start < end);
+    size_t len = end - start;
+    char* result = malloc((len + 1) * sizeof(char));
+
+    for(unsigned int resultIndex = 0, sourceIndex = start; resultIndex < len; ++resultIndex, ++sourceIndex) {
+        result[resultIndex] = str[sourceIndex];
+    }
+
+    result[len] = '\0';
+    return result;
+}
+
+
+int main(void) {
+
+    const char* str = "This is a string";
+
+    char* result = substr(str, 0, 4);
+    printf("%s \n", result);
+
+    char* result2 = substr(str, 5, 7);
+    printf("%s \n", result2);
+
+    char* result3 = substr(str, 8, 9);
+    printf("%s \n", result3);
+
+    char* result4 = substr(str, 10, 16);
+    printf("%s \n", result4);
+
+    free(result);
+    free(result2);
+    free(result3);
+    free(result4);
+
+    return 0;
+}
