9d505aa0fee05643c3da65105465018575fffc07
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-20 19:13:43 +0100

    read file content

--- streams.c
+++ streams.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+long getFileSize(FILE *fp)
+{
+    long result = 0;
+    // Sets the position indicator associated with the stream to a new position.
+    // Second parameter is the offset, third is the origin. Returns zero on success.
+    if (fseek(fp, 0, SEEK_END) == 0)
+    {
+        // Returns the current value of the position indicator (number of characters from the beginning) of the stream
+        result = ftell(fp);
+    }
+    // Reset position indicator
+    rewind(fp);
+    return result;
+}
 
 int main(void)
 {
@@ -18,18 +35,14 @@ int main(void)
         return 1;
     }
 
-    // Sets the position indicator associated with the stream to a new position.
-    // Second parameter is the offset, third is the origin. Returns zero on success.
-    int result = fseek(fp, 0, SEEK_END);
-
-    // Returns the current value of the position indicator of the stream.
-    long size = ftell(fp);
-
-    // Reset position indicator
-    rewind(fp);
-    long start = ftell(fp);
+    long size = getFileSize(fp);
+    assert(size > 0);
+    char *buff = malloc(size * sizeof(char));
+    // This also advances the file position indicator
+    size_t elementsRead = fread(buff, sizeof(char), size, fp);
 
     fclose(fp);
+    free(buff);
 
     return 0;
 }
