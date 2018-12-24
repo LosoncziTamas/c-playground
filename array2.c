d1446b4b9f312a1b5a6814905901a69fda75f049
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-24 21:19:23 +0100

    add realloc sample & update TODOs

---
+++ array2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void* safeMalloc(size_t len) 
+{
+    void* result = malloc(len);
+    if (!result) 
+    {
+        exit(1);
+    }
+    return result;
+}
+
+int main(void) {
+
+    const size_t arraySize = 16;
+    int* array = safeMalloc(sizeof(int) * arraySize);
+
+    for(int i = 0; i < arraySize; ++i) 
+    {
+        array[i] = i;
+        printf("%d", i);
+    }
+    puts("\n");
+
+    const size_t newSize = 32;
+    int* result = realloc(array, sizeof(int) * newSize);
+    // If there is not enough memory, the old memory block is not freed and null pointer is returned.
+    if (!result)
+    {
+        // The original pointer remains valid.
+        result = array;
+        puts("Realloc failed\n");
+    }
+
+    for(int i = 0; i < newSize; ++i) 
+    {
+        array[i] = i;
+        printf("%d", i);
+    }
+    puts("\n");
+    free(result);
+
+    //TODO: realloc in struct array
+}
