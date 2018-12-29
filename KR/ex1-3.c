6e95d25a61cf6ec382989af6f20440bd20ac7b1f
Tamás Losonczi <losonczi.tamas91@gmail.com>
2018-12-29 08:45:18 +0100

    add KR folder with exercise & update task config

---
+++ KR/ex1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+/* Fahrenheit-Celsius table 0 - 300 */
+
+int main()
+{
+    float fahr = LOWER;
+
+    while(fahr <= UPPER)
+    {
+        float celsius = (5.0f / 9.0f) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr += STEP;
+    }
+
+    return 0;
+}
