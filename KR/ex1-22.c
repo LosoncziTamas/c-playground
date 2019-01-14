fe2c748fdfc44ec664114c83cb39df40b098e62e
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-01-14 21:45:18 +0100

    add ex 1.22

---
+++ KR/ex1-22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*  Write a program to "fold" long input lines
+    into two or more shorter lines after the 
+    last non-blank character that occurs before
+    the n-th column of input. Make sure your 
+    program does something intelligent with 
+    very long lines, and if there are no blanks
+     or tabs before the specified column. */
+
+    // read til EOF
+    // count & store chars in array (n-sized)
+    // keep track of last blank & check limit
+    // if limit is over then print until the last blank (excluding it), print new line & move the remainder, update variables
+    // if limit is not over and new line, then print the array and clear variables
+    // if there are no blanks and limits is over, then split
+
+int main(void)
+{
+
+    return 0;
+}
