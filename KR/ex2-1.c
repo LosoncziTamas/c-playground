65f3daa0422140ba1044ac85e25edf396091f2ae
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-01-21 20:15:54 +0100

    finish ex 2.1

--- KR/ex2-1.c
+++ KR/ex2-1.c
@@ -52,11 +52,10 @@ void computeSizes()
     printf("signed char max: %d\n", maxSignedChar);
 
     // unsigned char
-    int unsignedSharSize = sizeof(unsigned char) * 8;
     unsigned char maxUnsignedChar = ~0;
 
-    printf("signed char min: %u ", 0);
-    printf("signed char max: %u\n", maxUnsignedChar);
+    printf("unsigned char min: %u ", 0);
+    printf("unsigned char max: %u\n", maxUnsignedChar);
 
     // signed short
     int shortSize = sizeof(short) * 8;
@@ -65,10 +64,43 @@ void computeSizes()
 
     printf("signed short min: %d ", minSignedShort);
     printf("signed short max: %d\n", maxSignedShort);
+
+    // unsigned short
+    unsigned short maxUnsignedShort = ~0;
+
+    printf("unsigned short min: %d ", 0);
+    printf("unsigned short max: %d\n", maxUnsignedShort);
+
+    // signed long
+    int longSize = sizeof(long) * 8;
+    long minSignedLong = (1L << (longSize - 1));
+    long maxSignedLong = ~(1L << (longSize - 1));
+
+    printf("signed long min: %ld ", minSignedLong);
+    printf("signed long max: %ld\n", maxSignedLong);
+
+    // unsigned long
+    unsigned long maxUnsignedLong = ~0;
+
+    printf("unsigned long min: %lu ", 0L);
+    printf("unsigned long max: %lu\n", maxUnsignedLong);
 }
 
 int main()
 {
+    printSizes();
+    putchar('\n');
     computeSizes();
+    putchar('\n');
+
+    printf("float min: %.10e ", FLT_MIN);
+    printf("float max: %.10e\n", FLT_MAX);
+
+    printf("double min: %.10e ", DBL_MIN);
+    printf("double max: %.10e\n", DBL_MAX);
+
+    printf("long double min: %.Le ", LDBL_MIN);
+    printf("long double max: %.Le\n", LDBL_MAX);
+
     return 0;
 }
