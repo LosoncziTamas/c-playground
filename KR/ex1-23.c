3cf3c3a0d86070a616c5eb52a6d8b61054207bab
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-01-24 07:59:42 +0100

    add ex 1.23 skeleton and test file

---
+++ KR/ex1-23.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+    Write a program to remove all
+    comments from a C program. 
+    Don't forget to handle quoted
+    strings and character constants properly. 
+    C comments do not nest.
+*/
+
+/*
+    Step 1: Read till EOF
+    Step 2: Identify comment start
+        a: Single line comment
+        b: Comment block
+    Step 3: Skip until line or block end.
+        
+*/
+
+int main(void)
+{
+    
+    return 0;
+}
