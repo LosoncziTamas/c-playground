fbeddbdc7af0604f3d14a4b93abbb730ad4e5dc5
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-01-27 11:43:14 +0100

    update ex 1.23 & add out file

---
+++ KR/ex1-23-out.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/
+
+/
+#define CONST 12
+
+int main()
+{
+    int a = 5;
+    for (int i = 0; i < a; ++i)
+    {
+        char* var = "/* string */";
+        char* var2 = "*/";
+        char* var3 = "// asdasd";
+
+        /
+        char c = '/';
+        if ( i % 2 == 0)
+        {
+            /            /
+            continue;
+        }
+    }
+}
+
+/
--- KR/ex1-23.c
+++ KR/ex1-23.c
@@ -8,25 +8,51 @@
     C comments do not nest.
 */
 
-/*
-    Step 1: Read till EOF
-    Step 2: Identify comment start
-        a: Single line comment
-        b: Comment block
-    Step 3: Skip until line or block end.
-        
-*/
-
 int main(void)
 {
-
     bool inQuote = false;
-    for (int c = getchar(); c != EOF; c = getchar())
+    bool inLineComment = false;
+    bool inBlockComment = false;
+
+    for (int c = getchar(), lastChar = 0; c != EOF; c = getchar())
     {
         if (c == '"')
         {
             inQuote = !inQuote;
         }
+
+        if (!inQuote && lastChar == '/')
+        {
+            if (c == '*')
+            {
+                inBlockComment = true;
+            }
+            else if (c == '/')
+            {
+                inLineComment = true;
+            }
+        }
+
+        if (inLineComment)
+        {
+            if (c == '\n')
+            {
+                inLineComment = false;
+            }
+        }
+        else if (inBlockComment)
+        {
+            if (lastChar == '*' && c == '/')
+            {
+                inBlockComment = false;
+            }
+        }
+        else 
+        {
+            putchar(c);
+        }
+        lastChar = c;
     }
+
     return 0;
 }
