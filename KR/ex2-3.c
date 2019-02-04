abf808ae118b210fb3b9c26f3da9e7efe5732339
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-02-04 08:06:20 +0100

    finish ex 2.3

--- KR/ex2-3.c
+++ KR/ex2-3.c
@@ -14,35 +14,33 @@ int htoi(char s[])
     for (int i = 0; s[i + 2] != '\0'; ++i)
     {
         char c = s[i + 2];
+        int val = 0;
         if (c >= 'A' && c <= 'F')
         {
-            
+            val = c - 55; 
         }
         else if (c >= 'a' && c <= 'f')
         {
-            
-
+            val = c - 87; 
         }
-        else if (c >= '1' && c <= '9')
+        else if (c >= '0' && c <= '9')
         {
-            
-        }
-        else if (c == '0')
-        {
-            
+            val = c - '0'; 
         }
         else 
         {
             return 0;
         }
+        result = 16 * result + val; 
     }
     return result;
 }
 
 int main(void)
 {
-    char* s = "0x10"; // 16
-    char* s2 = "0xFF"; // 255
+    char* s = "0x10"; 
+    char* s2 = "0xFF"; 
     htoi(s);
+    htoi(s2);
     return 0;
 }
