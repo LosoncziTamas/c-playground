0044727895ab4da16acaea3df7e454bf5ebbe1b6
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-02-11 08:02:10 +0100

    add ex 2.8

---
+++ KR/ex2-8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+
+/*  Write a function rightrot(x,n) 
+    that returns the value of the 
+    integer x rotated to the right
+    by n bit positions. */
+
+unsigned int rightrot(unsigned int x, unsigned int n)
+{
+    n = n % 32;
+    unsigned int captureMask = ~((~0U) >> n);
+    unsigned int result = (x >> n) | captureMask;
+
+    return result;
+}
+
+int main()
+{
+    assert(rightrot(0xFF, 3) == 0xE000001F);
+}
