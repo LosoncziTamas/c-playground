ce6551427676318f8ef39a748c6808dfcf59134c
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-02-14 07:13:34 +0100

    finish ex 2.9

--- KR/ex2-9.c
+++ KR/ex2-9.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 /*  In a two's complement number 
@@ -6,22 +7,25 @@
     Explain why. Use this observation 
     to write a faster version of bitcount. */
 
-/*
-    In two's complement system all odd numbers
+/*  In two's complement system all odd numbers
     have the rightmost set to 1.
     Subtracting one even from a negative number
     will always change the rightmost bit.
-    x = x & (x - 1); 
-
-*/
+    x = x & (x - 1); */
 
 int bitcount(unsigned x)
 {
-    x = x & (x - 1);
-    return 0;
+    int count = 0;
+    for (count = 0; x; ++count){
+        x = x & (x - 1);
+    }
+    return count;
 }
 
 int main()
 {
-
+    assert(bitcount(0xFFU) == 8);
+    assert(bitcount(0xFU) == 4);
+    assert(bitcount(0xC7U) == 5);
+    assert(bitcount(0) == 0);
 }
