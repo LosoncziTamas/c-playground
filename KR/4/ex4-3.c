d6d5df6d4877d27c62ef725d6647aa2306ba52b5
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-07-22 22:13:52 +0200

    ex 4.3 WIP second pass

--- KR/4/ex4-3.c
+++ KR/4/ex4-3.c
@@ -4,10 +4,13 @@
 #define NUMBER '0'
 
 int getops(char str[]);
+void push(double n);
+double pop();
 
 int main()
 {
     int type;
+    double op2;
     char s[MAXOP];
 
     while((type = getops(s)) != EOF) 
@@ -15,24 +18,34 @@ int main()
         switch (type)
         {
         case NUMBER:
-            
+            push(atof(s));
             break;
-        case '+':
-            
+        case '+':(
+            push(pop() + pop()));
             break;
         case '-':
-            
+            op2 = pop();
+            push(pop() - op2);
             break;
         case '*':
-            
+            push(pop() * pop());
             break;
         case '/':
-            
+            op2 = pop();
+            if (op2 != 0.0) 
+            {
+                push(pop() / op2);
+            }
+            else
+            {
+                printf("error: zero divisor\n");
+            }
             break;
         case '\n':
-            
+            printf("\t%.8g", pop());
             break;
         default:
+            printf("error: unknown command %s, \n", s);
             break;
         }
     }
