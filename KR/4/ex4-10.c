656870b3176e2bfe0c6fdd1d79bcac61c852a639
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-08-08 09:06:38 +0200

    ex 4.10 WIP

--- KR/4/ex4-10.c
+++ KR/4/ex4-10.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
-int getline(char s[], int limit);
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getline(char s[], int lim) 
+{
+    int c, i;
+
+    i = 0;
+    while(--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    {
+        s[i++] = c;
+    }
+    if (c == '\n')
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
 
 int getop(char s[])
 {
-
+    char c;
+    if (bufp == 0 || buf[bufp] == '\0')
+    {
+        bufp = 0;
+        // clear buffer
+        getline(buf, BUFSIZE);
+    }
+    
+    while((c = buf[bufp++]) == ' ' || c == '\t')
+        ;
+    // if (isdigit())
 }
