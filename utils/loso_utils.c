6e8c054324b36ce2923bb26f29a50cc6eebd4e3f
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-08-21 22:41:38 +0200

    utils WIP

--- utils/loso_utils.c
+++ utils/loso_utils.c
@@ -3,8 +3,34 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define ArrayCount(array) (sizeof(array)) / (sizeof((array)[0]))
+#define true 1
+#define false 0
+
+void TestArrayCount()
+{
+    int array[100]; 
+    assert(ArrayCount(array) == 100);
+
+    struct Foo 
+    {
+        char a;
+        char b;
+    };
+    struct Foo fooArray[50];
+    assert(ArrayCount(fooArray) == 50);
+
+    struct Bar
+    {
+        int a[20];
+    };
+    struct Bar bar;
+    assert(ArrayCount(bar.a) == 20);
+}
+
 typedef uint32_t uint32;
 typedef int32_t int32;
+typedef int32 bool32;
 
 void TestIntSize()
 {
@@ -12,7 +38,7 @@ void TestIntSize()
     assert(sizeof(int32) == 4);
 }
 
-uint32 StringLength(char* string)
+uint32 StringLength(const char* string)
 {
     uint32 result = 0;
 
@@ -24,7 +50,7 @@ uint32 StringLength(char* string)
     return result;
 }
 
-uint32 StringLengthSafe(char* string, uint32 max)
+uint32 StringLengthSafe(const char* string, uint32 max)
 {
     uint32 result = 0;
 
@@ -46,8 +72,34 @@ void TestStringLength()
     assert(StringLengthSafe(string, max) < max);
 }
 
+void StringConcat(const char* sourceA, uint32 sourceALen, const char* sourceB, uint32 sourceBLen, char* dest)
+{
+    while(true)
+    {
+        if(*sourceA++)
+        {
+            
+        }
+        else if(*sourceB++)
+        {
+
+        }
+    }
+}
+
+void TestStringConcat()
+{
+    char sourceA[] = "1234";
+    char sourceB[] = "5678";
+    char dest[10];
+
+    // StringConcat(sourceA, 4, sourceB, 4, dest);
+}
+
 int main(void) 
 {
+    TestArrayCount();
     TestIntSize();
     TestStringLength();
+    TestStringConcat();
 }
