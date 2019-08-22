bacb6e3aadb30b2a8ed6d8cba6081d626f219cac
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-08-22 17:57:53 +0200

    ex 4.12 WIP

--- KR/4/ex4-12.c
+++ KR/4/ex4-12.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
-#include "..\..\utils\loso_utils.c"
+#include <math.h>
+#include "../../utils/loso_utils.c"
 
 /*
     Write a recursive itoa.
 */
 
-void IntegerToString(uint32 integer, char string[])
+void IntegerToString(int32 integer, char string[])
 {
+    if (integer == 0)
+    {
+        *string = '0' + integer;
+    }
+    else
+    {
+        int32 tmp = integer; //1234
+        int32 base = 10;
+        uint32 magnitude = 0;
 
+        while (tmp = tmp / base) //0
+        {
+            magnitude++; //3
+        }
+        int32 exponent = pow(10.0, magnitude);
+        *string = '0' + integer / exponent;
+        IntegerToString(integer % exponent, ++string); 
+    }
 }
 
 int main()
 {
-    
+    char string[5];
+    IntegerToString(1234, string);
 }
