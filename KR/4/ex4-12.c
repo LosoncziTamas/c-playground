6f805cce9fbe2f60dc522c4a491771fff2026fc3
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-08-26 19:24:29 +0200

    ex 4.12 WIP

--- KR/4/ex4-12.c
+++ KR/4/ex4-12.c
@@ -8,45 +8,35 @@
 
 void IntegerToString(int32 integer, char string[])
 {
-    if (integer < 0)
+    static int32 i = 0;
+
+    if (integer / 10) 
     {
-        *string = '-';
-        string = &string[1];
-        //TODO: How to handle -INT_MIN
-        IntegerToString(-integer, string);
+        i++;
+        IntegerToString(integer / 10, string); //4678, 467, 46
+    }
+    else 
+    {
+        string[0] = '0' + integer; // 4
+        PrintChar(string[0]);
         return;
     }
-    if (integer == 0)
-    {
-        *string = '\0';
-    }
-    else
-    {
-        int32 tmp = integer;
-        int32 base = 10;
-        uint32 magnitude = 0;
-
-        while (tmp = tmp / base)
-        {
-            magnitude++;
-        }
-        int32 exponent = pow(10.0, magnitude);
-        *string = '0' + integer / exponent;
-        string = &string[1];
-        IntegerToString(integer % exponent, string); 
-    }
+    string[i--] = '0' + integer / 10; //
 }
+
+
 
 int main()
 {
     char string[10]; 
     IntegerToString(4678, string);
-    assert(StringsAreEqual("4678", string));
+    PrintCharArray(string, 5);
+    //assert(StringsAreEqual("4678", string));
 
-    IntegerToString(-123, string);
-    assert(StringsAreEqual("-123", string));
+    IntegerToString(123, string);
+    //assert(StringsAreEqual("123", string));
 
     //TODO: Fix
     IntegerToString(0, string);
-    assert(StringsAreEqual("0", string));
+    //assert(StringsAreEqual("0", string));
 }
