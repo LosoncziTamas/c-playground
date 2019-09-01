1536d0c2f46d4596a6999e6126193a5e4c7f3d68
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-09-01 08:58:37 +0200

    update ex 4.14

--- KR/4/ex4-14.c
+++ KR/4/ex4-14.c
@@ -5,13 +5,17 @@
     Define swap macro.
 */
 
-#define Swap(t, x, y) t tmp = (x);\
-                        (x) = (y);\
-                        (y) = tmp;
+#define Swap(t, x, y) {t tmp = (x); (x) = (y); (y) = tmp;}
+
 int main()
 {
     int x = 5;
     int y = 6;
     Swap(int, x, y);
     assert(y == 5 && x == 6);
+
+    int* a = &x;
+    int* b = &y;
+    Swap(int*, a, b);
+    assert(*a == 5 && *b == 6);
 }
