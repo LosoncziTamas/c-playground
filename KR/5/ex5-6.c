98ef0724ebae7fdd8c4cfbdc9a79112bf409414c
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-09-10 09:18:51 +0200

    update ex 5.6

--- KR/5/ex5-6.c
+++ KR/5/ex5-6.c
@@ -21,6 +21,14 @@ int32 GetLine(char *s, int32 lim)
     return s - start;
 }
 
+void TestGetLine()
+{
+    char buf[11];
+    int32 len = GetLine(buf, 11);
+    PrintInteger(len);
+    PrintCharArray(buf, 10);
+}
+
 int32 StringToInt32(char *s)
 {
     int32 result = 0;
@@ -41,34 +49,92 @@ int32 StringToInt32(char *s)
     return sign * result;
 }
 
-void ReverseString(char *str)
-{
-    uint32 len = StringLength(str);
-    char *head = str;
-    char *rear = str + len;
-    
-    char tmp = *str;
-    
-}
-
-
-void TestGetLine()
-{
-    char buf[11];
-    int32 len = GetLine(buf, 11);
-    PrintInteger(len);
-    PrintCharArray(buf, 10);
-}
-
 void TestStringToInt32()
 {
     assert(12345 == StringToInt32("12345"));
     assert(12345 == StringToInt32("012345"));
     assert(-12345 == StringToInt32("-12345"));
+}
 
+void ReverseString(char *str)
+{
+    uint32 len = StringLength(str);
+    char *start = str;
+    char *end = str + len - 1;
+    
+    while (end - start > 0)
+    {
+        char tmp = *start;
+        *(start++) = *end;
+        *(end--) = tmp;
+    }
+}
+
+void TestReverseString()
+{
+    {
+        char str[10] = "almafa";
+        ReverseString(str);
+        assert(StringsAreEqual("afamla", str));
+    }
+    {
+        char str[10] = "123";
+        ReverseString(str);
+        assert(StringsAreEqual("321", str));
+    }
+        {
+        char str[10] = "a";
+        ReverseString(str);
+        assert(StringsAreEqual("a", str));
+    }
+}
+
+void Int32ToString(int32 n, char* s)
+{
+    int32 sign = n;
+    char *start = s;
+
+    if (sign < 0)
+    {
+        //TODO: fix largest negative number
+        n = -n;
+    }
+
+    do
+    {
+        *(s++) = n % 10 + '0';
+    } 
+    while ((n /= 10) > 0);
+
+    if (sign < 0)
+    {
+        *(s++) = '-';
+    }
+    *s = '\0';
+
+    ReverseString(start);
+}
+
+void TestInt32ToString()
+{
+    {
+        int32 n = 12345;
+        char s[10] = {0};   
+        Int32ToString(n, s);
+        assert(StringsAreEqual(s, "12345"));
+    }
+    {
+        int32 n = 1;
+        char s[10] = {0};   
+        Int32ToString(n, s);
+        assert(StringsAreEqual(s, "1"));
+    }
 }
 
 int main()
 { 
-    TestStringToInt32();
+    // TestGetLine();
+    // TestReverseString();
+    // TestStringToInt32();
+    // TestInt32ToString();
 }
