343d965604e0b9b94ebf1a66aaad14f2e8356978
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-10-12 12:34:10 +0200

    [window] win32 offscreen buffer WIP

--- misc/window/win32_build.bat
+++ misc/window/win32_build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-gcc -c common.c -o common.o
-gcc -c window_win32.cpp -o window_win32.o
-gcc common.o window_win32.o -o window.exe
+gcc -Wall -c common.c -o common.o
+gcc -Wall -c window_win32.cpp -o window_win32.o
+gcc -Wall common.o window_win32.o -o window.exe -mwindows
--- misc/window/window_win32.cpp
+++ misc/window/window_win32.cpp
@@ -10,6 +10,35 @@ extern "C"
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+struct OffscreenBuffer
+{
+    BITMAPINFO info;
+    void *memory;
+    int width;
+    int height;
+    int pitch;
+};
+
+struct WindowSize
+{
+    int width;
+    int height;
+};
+
+static OffscreenBuffer globalbuffer;
+
+WindowSize GetWindowSize(HWND window)
+{
+    WindowSize result = {};
+
+    RECT clientRect;
+    GetClientRect(window, &clientRect);
+    result.width = clientRect.right - clientRect.left;
+    result.height = clientRect.bottom - clientRect.top;
+
+    return result;
+}
+
 WNDCLASS CreateWindowClass(HINSTANCE hInstance)
 {
     WNDCLASS result = {};
@@ -19,6 +48,39 @@ WNDCLASS CreateWindowClass(HINSTANCE hInstance)
     result.lpszClassName = L"className";
 
     return result;
+}
+
+void Win32ResizeDIBSection(OffscreenBuffer *buffer, int width, int height)
+{
+    if(buffer->memory)
+    {
+        VirtualFree(buffer->memory, 0, MEM_RELEASE);
+    }
+
+    buffer->width = width;
+    buffer->height = height;
+
+    int BytesPerPixel = 4;
+
+    buffer->info.bmiHeader.biSize = sizeof(buffer->info.bmiHeader);
+    buffer->info.bmiHeader.biWidth = buffer->width;
+    buffer->info.bmiHeader.biHeight = -buffer->height;
+    buffer->info.bmiHeader.biPlanes = 1;
+    buffer->info.bmiHeader.biBitCount = 32;
+    buffer->info.bmiHeader.biCompression = BI_RGB;
+
+    int BitmapmemorySize = (buffer->width*buffer->height)*BytesPerPixel;
+    buffer->memory = VirtualAlloc(0, BitmapmemorySize, MEM_COMMIT, PAGE_READWRITE);
+    buffer->pitch = width*BytesPerPixel;
+}
+
+void RenderStuffTobuffer(OffscreenBuffer *buffer)
+{
+    for (int i = 0; i < buffer->height * buffer->width; ++i)
+    {
+        char* pixelMem = (char*)buffer->memory + i;
+        *pixelMem = 255;
+    }
 }
 
 HWND CreateSimpleWindow(WNDCLASS *wndClass, HINSTANCE hInstance)
@@ -38,7 +100,6 @@ HWND CreateSimpleWindow(WNDCLASS *wndClass, HINSTANCE hInstance)
     return result;
 }
 
-
 INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
     WNDCLASS wndClass = CreateWindowClass(hInstance);
@@ -52,7 +113,8 @@ INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nC
     }
 
     ShowWindow(window, nCmdShow);
-
+    WindowSize winSize = GetWindowSize(window);
+    Win32ResizeDIBSection(&globalbuffer, winSize.width, winSize.height);
     bool running = true;
 
     MSG msg = {};
@@ -81,21 +143,17 @@ LRESULT CALLBACK WindowProc(HWND window, UINT uMsg, WPARAM wParam, LPARAM lParam
         case WM_PAINT:
         {
             PAINTSTRUCT paint;
-            HDC deviceContext = BeginPaint(window, &paint);
+            WindowSize winSize = GetWindowSize(window);
+            HDC deviceContext = BeginPaint(window, &paint);        
+                RenderStuffTobuffer(&globalbuffer);
 
-            RECT clientRect;
-            GetClientRect(window, &clientRect);
-            int width = clientRect.right - clientRect.left;
-            int height = clientRect.bottom - clientRect.top;
-
-            /*
             StretchDIBits(deviceContext,
-                0, 0, width, height,
-                0, 0, width, height,
-                Buffer->Memory,
-                &Buffer->Info,
+                0, 0, winSize.width, winSize.height,
+                0, 0, winSize.width, winSize.height,
+                globalbuffer.memory,
+                &globalbuffer.info,
                 DIB_RGB_COLORS, SRCCOPY);
-            */
+            
             EndPaint(window, &paint);
         }
         return 0;
