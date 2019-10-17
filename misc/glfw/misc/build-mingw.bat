4eb3530f1aec17c423e46577ada531d92a4b69cb
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-10-17 20:32:45 +0200

    [glfw] update win bat file

--- misc/glfw/misc/build-mingw.bat
+++ misc/glfw/misc/build-mingw.bat
@@ -1,2 +1,3 @@
-
-gcc main.c -Iinclude -Llib-mingw -lglfw3dll
+rem TODO: use 32 bit mingw version, or try MSVC: https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=vs-2019
+rem "Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd"
+gcc main.c -m32 -Iinclude -Llib-mingw -lglfw3
