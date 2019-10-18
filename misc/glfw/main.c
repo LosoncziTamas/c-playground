7990843d72c962c949fb4acca02d34e3f74edb3b
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-10-18 17:49:08 +0200

    [glfw] read input

--- misc/glfw/main.c
+++ misc/glfw/main.c
@@ -1,36 +1,66 @@
+#include <stdio.h>
 #include "GLFW/glfw3.h"
+
+typedef struct 
+{
+    int leftButtonPressed;
+    int rightButtonPressed;
+    int upButtonPressed;
+    int downButtonPressed;
+    int escButtonPressed;
+} Input;
+
+typedef struct
+{
+    Input input;
+} AppState;
+
+void ErrorCallback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+Input ReadInput(GLFWwindow* window)
+{
+    Input result = {};
+
+    result.leftButtonPressed = glfwGetKey(window, GLFW_KEY_LEFT);
+    result.rightButtonPressed = glfwGetKey(window, GLFW_KEY_RIGHT);
+    result.downButtonPressed = glfwGetKey(window, GLFW_KEY_DOWN);
+    result.upButtonPressed = glfwGetKey(window, GLFW_KEY_UP);
+    result.escButtonPressed = glfwGetKey(window, GLFW_KEY_ESCAPE);
+
+    return result;
+}
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    if (glfwInit())
     {
+        glfwSetErrorCallback(ErrorCallback);
+        
+        GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW", NULL, NULL);
+        AppState state = {0};
+
+        if (window)
+        {
+            glfwMakeContextCurrent(window);
+
+            while (!glfwWindowShouldClose(window))
+            {
+                glClear(GL_COLOR_BUFFER_BIT);
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+
+                state.input = ReadInput(window);
+                if (state.input.escButtonPressed)
+                {
+                    glfwSetWindowShouldClose(window, GLFW_TRUE);
+                }
+            }
+        }
         glfwTerminate();
-        return -1;
     }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
 }
