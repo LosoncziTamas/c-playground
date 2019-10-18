5e869c3dddd104e39bb1dcc5fe45ea48691555ac
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-10-18 09:26:41 +0200

    [glfw] switch to use 64 bit libraries, update ms build script

--- misc/glfw/misc/build-mingw.bat
+++ misc/glfw/misc/build-mingw.bat
@@ -1,3 +1,3 @@
 rem TODO: use 32 bit mingw version, or try MSVC: https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line?view=vs-2019
 rem "Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd"
-gcc main.c -m32 -Iinclude -Llib-mingw -lglfw3
+gcc main.c -o main.exe -Iinclude -Llib-mingw -lglfw3 -lopengl32
