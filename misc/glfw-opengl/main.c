c1563791855f1997eaae07b93a8ebd82c5563a87
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-10-22 09:21:33 +0200

    [glfw-opengl] draw triangle

--- misc/glfw-opengl/main.c
+++ misc/glfw-opengl/main.c
@@ -3,6 +3,26 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+const char* VertexShader = 
+"#version 330 core\n\
+layout (location = 0) in vec3 aPos; \
+ \
+void main() \
+{ \
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \
+}";
+
+const char* FragmentShader =
+"#version 330 core\n\
+out vec4 FragColor; \
+ \
+void main() \
+{ \
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \
+} ";
+
+static char InfoLog[128];
+
 void ErrorCallback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -21,6 +41,33 @@ void FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void CheckShaderState(GLuint shader, GLFWwindow* window)
+{
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        printf(infoLog);
+    }
+}
+
+void CheckProgramState(GLuint program, GLFWwindow* window)
+{
+    int  success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(program, 512, NULL, infoLog);
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        printf(infoLog);
+    }
+}
+
+
 int main(void)
 {
     glfwInit();
@@ -32,7 +79,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW - OpenGL", NULL, NULL);
 
     if (window)
     {   
@@ -43,10 +90,65 @@ int main(void)
 
         if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         {
+            float vertices[] = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f,  0.5f, 0.0f
+            }; 
+            
+            GLuint vbo;
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+            GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, 1, &VertexShader, NULL);
+            glCompileShader(vertexShader);
+            CheckShaderState(vertexShader, window);
+
+            GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, 1, &FragmentShader, NULL);
+            glCompileShader(fragmentShader);
+            CheckShaderState(fragmentShader, window);
+
+            GLuint shaderProgram = glCreateProgram();
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+            glLinkProgram(shaderProgram);
+
+            glDeleteShader(fragmentShader);
+            glDeleteShader(vertexShader);
+            CheckProgramState(shaderProgram, window);
+
+            glUseProgram(shaderProgram);
+
+            GLuint vao;
+            glGenVertexArrays(1, &vao);
+            glBindVertexArray(vao);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+            // Specifying how to interpret the provided data
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            // Enable vertex attribute at location 0
+            glEnableVertexAttribArray(0);
+
             while (!glfwWindowShouldClose(window))
             {
                 glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
+
+                glBindBuffer(GL_ARRAY_BUFFER, vbo);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+                glEnableVertexAttribArray(0);  
+
+                glUseProgram(shaderProgram);
+                glBindVertexArray(vao);
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+
                 glfwSwapBuffers(window);
                 glfwPollEvents();
             }
