df61e0148a8b956b002220914e87bf1ae967fb21
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-12-21 16:47:46 +0000

    [ex-5.13] update test.sh

--- KR/5/5.13/test.sh
+++ KR/5/5.13/test.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# compile
-cc tail.c -Wall -o tail.out
-
 # feed test data
-test1src=`cat test1.input`
-./tail.out -2 $test1src > test1.result
-diff test1.result test1.expected
+./tail.out -2 "first loie"$'\n'"second line"$'\n'"third line"$'\n'"last line"
