d60d3f3626b49c8def228cf98bc3cd322830d137
Tamás Losonczi <losonczi.tamas91@gmail.com>
2020-08-24 21:54:28 +0200

    [ex-5.14] add quick sort & comparators

--- KR/5/5.14/sort.c
+++ KR/5/5.14/sort.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 
 #define MAXLEN 1000
+#define MAXLINES 5000
+
+char* lines[MAXLINES];
 
 char* Alloc(int size)
 {
@@ -51,19 +54,68 @@ int ReadLines(char* linePtr[], int maxLines)
     return lineCount;
 }
 
-#define MAXLINES 5000
+void WriteLines(char* linePtr[], int lineCount)
+{
+    for (int i = 0; i < lineCount; ++i)
+    {
+        printf("%s\n", linePtr[i]);
+    }
+}
 
-char* lines[MAXLINES];
+int NumberComparator(char* s1, char* s2)
+{
+    double a = atof(s1);
+    double b = atof(s2);
+
+    return a < b ? -1 : (a > b ? 1 : 0);
+}
+
+int StringComparator(char* s1, char* s2)
+{
+    return strcmp(s1, s2);
+}
+
+void Swap(char* v[], int i, int j)
+{
+    void* temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void QuickSort(char *data[], int left, int right, int (*comparator)(char*, char*))
+{
+    if (left >= right)
+    {
+        return;
+    }
+    Swap(data, left, (left + right) / 2);
+    int last = left;
+    for (int i = left + 1; i <= right; ++i)
+    {
+        if (comparator(data[i], data[left]) < 0)
+        {
+            Swap(data, ++last, i);
+        }
+    }
+    Swap(data, left, last);
+    QuickSort(data, left, last - 1, comparator);
+    QuickSort(data, last + 1, right, comparator);
+}
 
 int main(int argc, char* argv[])
 {
     bool isNumeric = argc > 1 && strcmp(argv[1], "-n") == 0;
     int lineCount = ReadLines(lines, MAXLINES);
 
-    for (int i = 0; i < lineCount; ++i)
+    if (lineCount > 0)
     {
-        printf("lines[%d] %s \n", i, lines[i]);
+        QuickSort(lines, 0, lineCount - 1, isNumeric ? NumberComparator : StringComparator);
+        WriteLines(lines, lineCount);
+        return 0;
     }
 
-    return 0;
+    printf(lineCount == 0 ? "No input to sort.\n" : "Input too big to sort.\n");
+    return 1;
 }
